4f121045e901cf0553e41a2b9310f18a229e4e8d
duyhd <duyhd@gemvietnam.com>
2022-03-18 22:22:20 +0700

    add [Bài tập] Simple GUI calculator using Tkinter

--- new.py
+++ new.py
@@ -1,22 +1,3 @@
-class MyNumbers:
-    def __iter__(self):
-        self.a = 2
-        return self
-
-    def __next__(self):
-        x = self.a
-        self.a += 2
-        if (self.a < 11):
-            return x
-        else:
-            StopIteration
-
-
-mynumber = MyNumbers()
-inumber = iter(mynumber)
-
-print(next(inumber))
-print(next(inumber))
-print(next(inumber))
-print(next(inumber))
-print(next(inumber))
+import test
+a = test.person1['age']
+print(a)
